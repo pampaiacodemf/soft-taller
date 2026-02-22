bc878ede407c29c3b866155ce647334387f24cfc
pampaiacodemf <pampaiacodemf@gmail.com>
2026-02-22 18:28:06 -0300

    Fix: Export explicit env vars in next.config.mjs for Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,11 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NEXTAUTH_URL: "https://soft-taller.netlify.app",
+    AUTH_URL: "https://soft-taller.netlify.app",
+    AUTH_SECRET: "f3f1e9c2b4a5d6e7f8g9h0i1j2k3l4m5n6o7p8q9r0s1t2u3v4w5x6y7z8a9b0c1",
+  },
 };
 
 export default nextConfig;
